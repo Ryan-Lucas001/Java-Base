eb5a47783c12affdecb78efb1ce2dd12c5ac8b59
Ryan Lucas <rryanlucasmorais@gmail.com>
2025-07-07 11:02:06 -0300

    Create: Ternario.java

---
+++ Ternario.java
@@ -0,0 +1,15 @@
+package fundamentos.operadores;
+
+public class Ternario {
+    public static void main(String[] args) {
+
+        double media = 6.0;
+
+        String resultadoParcial = media >= 5 ? "em recuperação" : "reprovado.";
+
+
+        String resultadoFinal = media >= 7.0 ? "Aprovado." : resultadoParcial;
+
+        System.out.println("O aluno está " + resultadoFinal );
+    }
+}
