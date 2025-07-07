9bec75f4bcabca3ddd7b6a91a74dacaf2fc6a7b7
Ryan Lucas <rryanlucasmorais@gmail.com>
2025-07-07 10:21:05 -0300

    Create: DesafioCauculadora.java

---
+++ DesafioCauculadora.java
@@ -0,0 +1,36 @@
+package fundamentos;
+
+import java.util.Scanner;
+
+public class DesafioCalculadora {
+    public static void main(String[] args) {
+
+        Scanner entrada = new Scanner(System.in); // Para que o úsuario coloque seus dados 
+
+        System.out.println("Informe o número: ");
+        double num1 = entrada.nextDouble();
+
+        System.out.println("Informe outro número: ");
+        double num2 = entrada.nextDouble();
+
+        System.out.println("Informe a operação: ");
+        String operaçao = entrada.next();
+
+        // Lógica
+
+        double resultado = "+".equals(operaçao) ? num1 + num2 : 0;
+
+        resultado = "-".equals(operaçao) ? num1 - num2 : resultado; // : = caso contrario
+
+        resultado = "".equals(operaçao) ? num1 num2 : resultado;
+
+        resultado = "/".equals(operaçao) ? num1 / num2 : resultado;
+
+        resultado = "%".equals(operaçao) ? num1 % num2 : resultado;
+
+        System.out.printf("%.2f %s %.2f = %.2f", num1, operaçao, num2, resultado);
+
+        entrada.close();
+
+    }
+}
