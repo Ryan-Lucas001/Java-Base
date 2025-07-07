220b0eb925817f78108f836bb3bdbcf203ecca01
Ryan Lucas <rryanlucasmorais@gmail.com>
2025-07-07 10:51:49 -0300

    Create: DesafioLógico.java

---
+++ DesafioLogico.java
@@ -0,0 +1,39 @@
+package fundamentos.operadores;
+
+public class DesafiosLogicos {
+
+    public static void main(String[] args) {
+
+        // Trabalho na terça (v ou f)
+
+        // Trabalho na quinta (v ou F)
+
+        // Operadorres Binários 
+
+        boolean trabalho1 = false;
+
+        boolean trabalho2 = false;
+
+        boolean comprouTv50 = trabalho1 && trabalho2;
+
+        boolean comprouTV32 = trabalho1 ^ trabalho2;
+
+        boolean comprouSorvete = trabalho1 || trabalho2;
+
+
+        System.out.println("Comprou TV 50"? "  comprouTv50);
+        System.out.println("Comprou Tv32"? " + comprouTV32);
+        System.out.println("Comprou TV 32"Sorvete? " + comprouSorvete);
+
+        // Operador Unário 
+
+        System.out.println("Mais Saúdavel"? " + !comprouSorvete );
+
+
+
+
+
+
+    }
+
+}
