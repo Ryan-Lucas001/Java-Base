c9bcda209df56be1e6c5f02de4827412c756ff76
Ryan Lucas <rryanlucasmorais@gmail.com>
2025-07-07 11:02:50 -0300

    Create: Unarios.java

---
+++ Unarios.java
@@ -0,0 +1,26 @@
+public class Unarios {
+    public static void main(String[] args) {
+
+        // FAZER CÓDIGOS SIMPLES PARA TER UM BOM ENTENDIMENTO 
+
+       int a = 1;
+       int b = 2;
+
+       a++; // Para Acrementar = ++ = (a = a + 1)
+       a--; // Para Decrementar = -- = (a = a - 1)
+
+       ++b; // b = b + 1;
+       --b; // b = b - 1;
+
+       System.out.println(a);
+       System.out.println(b);
+
+       System.out.println("Mini desafio...");
+
+        // Mistura de incremento com decremento 
+
+       System.out.println(++a == b--);
+
+
+    }
+}
